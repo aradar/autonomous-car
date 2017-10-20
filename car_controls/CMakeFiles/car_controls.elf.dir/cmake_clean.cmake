f8a6947b8296ee7ed9aba0ce3a5def9c0b5619c2
Bluemi <bruno.schilling@protonmail.ch>
2017-10-20 18:41:46 +0200

    + build.sh for automated cmake build changed CMakeLists.txt redefined build path included libs for blink-LED added libs in Drivers/

---
+++ car_controls/CMakeFiles/car_controls.elf.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/car_controls.elf.dir/Src/main.c.obj"
+  "CMakeFiles/car_controls.elf.dir/Src/stm32f3xx_hal_msp.c.obj"
+  "CMakeFiles/car_controls.elf.dir/Src/stm32f3xx_it.c.obj"
+  "CMakeFiles/car_controls.elf.dir/Src/system_stm32f3xx.c.obj"
+  "CMakeFiles/car_controls.elf.dir/Drivers/STM32F3xx_Nucleo_32/stm32f3xx_nucleo_32.c.obj"
+  "CMakeFiles/car_controls.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal.c.obj"
+  "CMakeFiles/car_controls.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc.c.obj"
+  "CMakeFiles/car_controls.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_cortex.c.obj"
+  "CMakeFiles/car_controls.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_gpio.c.obj"
+  "car_controls.elf.pdb"
+  "car_controls.elf"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/car_controls.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ car_controls/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+if [ "$1" == "-c" ]; then
+	cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=STM32F303x8.cmake -G "CodeBlocks - Unix Makefiles" ./
+fi
+make -f CMakeFiles/Makefile2 all
