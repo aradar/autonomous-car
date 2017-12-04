dce965c405ba53071978c3d785a8c9c0d7b589bf
Bluemi <bruno.schilling@protonmail.ch>
2017-12-04 09:18:00 +0100

    merged Packtizer

--- main_unit/ai_robo_car/ai_robo_car/packet.py
+++ main_unit/ai_robo_car/ai_robo_car/packet.py
@@ -1,66 +1,56 @@
+#!/usr/bin/env python3
+
+# import serial
+import sys
 import struct
 from enum import Enum
 
 ##############################################
 #
-# to create a new Packet do the following:
+# to create data you can send via serial do the following:
 #
-# 	packet = Packet()
-# 	packet.mode = <mode> 			# where <mode> is one of "Mode.DRIVE" or "Mode.STEER"
-# 	packet.direction = <direction> 		# where <direction> is one of "Direction.BACKWARD" or "Direction.FORWARD"
-# 	packet.debug_lvl = <debug_level> 	# where <debug_lvl> is an int with: 0 <= debug_lvl < 64
-# 	packet.value = <value> 			# where <value> should be better defined in the next version :P
+# 	data = Packetizer.create_data(<mode>, <direction>, <debug_lvl>, <value>) # where <mode> is one of "Mode.DRIVE" or "Mode.STEER"
+# 									 	 # where <direction> is one of "Direction.BACKWARD" or "Direction.FORWARD"
+# 									 	 # where <debug_lvl> is an int with: 0 <= debug_lvl < 64
+# 									 	 # where <value> should be better defined in the next version :P
+# then you can send "data" via serial:
 #
-# or use:
-#	packet = Packet()
-#	packet.setData(<mode>, <direction>, <debug_lvl>, <value>)
+# 	ser.write(data)
 #
-# to send this packet do:
+# or, if you want to send, directly use:
 #
-# 	serial.write(packet.getData())
+#	Packetizer.write_data(<serial>, <mode>, <direction>, <debug_lvl>, <value>) # where serial is the serial port over which the data should be sent
 #
 ##############################################
 
-NULL_MASK = 0b00000000
-MODE_MASK = 0b10000000
-DIRECTION_MASK = 0b01000000
-OPTION_MASK = 0b00111111
-
+NULL_MASK 	= 0b00000000
+MODE_MASK 	= 0b10000000
+DIRECTION_MASK 	= 0b01000000
+OPTION_MASK 	= 0b00111111
 
 class Mode(Enum):
-    STEER = 0
-    DRIVE = 1
-
+	STEER = 0
+	DRIVE = 1
 
 class Direction(Enum):
-    FORWARD = 0
-    BACKWARD = 1
+	FORWARD = 0
+	BACKWARD = 1
 
+class Packetizer:
+	def create_header(mode, direction, debug_lvl):
+		header = 0
+		header |= MODE_MASK if self.mode == Mode.DRIVE else NULL_MASK
+		header |= DIRECTION_MASK if self.direction == Direction.BACKWARD else NULL_MASK
+		header |= OPTION_MASK & self.debug_lvl
+		return header
 
-class Packet:
-    mode = Mode.STEER
-    direction = Direction.FORWARD
-    debug_lvl = 0
-    value = 0
+	def create_value(value):
+		return int(value)
 
-    def setData(self, mode_, direction_, debug_lvl_, value_):
-        self.mode = mode_
-        self.direction = direction_
-        self.debug_lvl = debug_lvl_
-        self.value = value_
+	def create_data(mode, direction, debug_lvl, value):
+		header = create_header(mode, direction, debug_lvl)
+		val = create_value(value)
+		return struct.pack("<Bi", header, val)
 
-    def fromData(data):
-        print("Packet::fromData(): TODO")
-
-    def getData(self):
-        return struct.pack("<Bi", self.getHeader(), self.getValue())
-
-    def getHeader(self):
-        header = 0
-        header |= MODE_MASK if self.mode == Mode.DRIVE else NULL_MASK
-        header |= DIRECTION_MASK if self.direction == Direction.BACKWARD else NULL_MASK
-        header |= OPTION_MASK & self.debug_lvl
-        return header
-
-    def getValue(self):
-        return int(self.value)
+	def write_data(serial, mode, direction, debug_lvl, value):
+		serial.write(create_data(mode, direction, debug_lvl, value))
