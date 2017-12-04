723e49e56d06df82a248d15b157f6e41365beff5
cap <phandoom8@gmail.com>
2017-12-04 16:50:15 +0100

    added threshold and check for x=0 and y=0

--- main_unit/ai_robo_car/ai_robo_car/layer/path_translator.py
+++ main_unit/ai_robo_car/ai_robo_car/layer/path_translator.py
@@ -3,31 +3,37 @@ from ai_robo_car.layer.data_objects import TargetPoint
 from ai_robo_car.layer.data_objects import EngineInstruction
 import math
 
+
 class PathTranslator(AbstractLayer[TargetPoint, EngineInstruction]):
     def call_from_upper(self, message: TargetPoint) -> None:
         (x, y) = message.position
         print("PathTranslator: call_from_upper ->", x, y)
 
-        if x == 0 and y == 0:
-            print("INVALID POINT:", x, y)
-            return
+        steer = 0
+        speed = 0
+        threshold_angle = 30
+        if (x != 0) or (y != 0):
 
-        if x != 0:
-            angle = math.atan(y / x) / math.pi * 180
-        else:
-            angle = 90
+            if x != 0:
+                angle = (math.atan(y / x) * 180) / math.pi
+            else:
+                angle = 90
 
-        if angle < -90 or angle > 90:
-            print("INVALID ANGLE:", angle)
-            return
+            if angle < -90 or angle > 90:
+                print("INVALID ANGLE:", angle)
+                angle = 0
+            elif 0 > angle > -threshold_angle:
+                angle = -threshold_angle
+            elif 0 < angle < threshold_angle:
+                angle = threshold_angle
 
-        print("using angle:", angle)
+            print("using angle:", angle)
 
-        speed = math.fabs((angle + 90) / 90 - 1)
-        if angle > 0:
-            steer = 90 - angle
-        else:
-            steer = -90 - angle
+            speed = math.fabs((angle + 90) / 90 - 1)
+            if angle > 0:
+                steer = 90 - angle
+            elif angle < 0:
+                steer = -90 - angle
 
         if self.lower is not None:
             self.call_lower(EngineInstruction(speed, steer))
