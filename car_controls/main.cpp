f6ef67a82f0969ef440b8fe963c9946de46e79ed
MaxiGH <maxigertz@googlemail.com>
2017-12-08 13:17:00 +0100

    implemented controller functionality

--- car_controls/main.cpp
+++ car_controls/main.cpp
@@ -1,9 +1,9 @@
 #include "mbed.h"
 #include "Servo.h"
 
-uint8_t const MODE_MASK = 0x80;
+uint8_t const SIDE_MASK = 0x80;
 uint8_t const DIRECTION_MASK = 0x40;
-uint8_t const DEBUG_MASK = ~ (MODE_MASK | DIRECTION_MASK);
+uint8_t const DEBUG_MASK = ~ (SIDE_MASK | DIRECTION_MASK);
 
 bool received_something = false;
 int wait_millis = 0;
@@ -13,44 +13,49 @@ Serial pi(PB_6, PB_7, 9600); //UART1_TX / D4, UART1_RX / D5
 DigitalOut statusLed(LED1);
 
 struct SerialInputProtocol {
-    enum {STEER, DRIVE} mode;
-    enum {FORWARD, BACKWARD} direction;
+    enum {LEFT, RIGHT} side;
+    enum {BACKWARD, FORWARD} direction;
 
     char debugLevel;
-    float value;
+    float valueSteer;
+    float valueSpeed;
 
     static SerialInputProtocol read(uint8_t* buffer) {
         SerialInputProtocol result;
         
         uint8_t options = buffer[0];
-        result.mode = (options & MODE_MASK) == MODE_MASK ? DRIVE : STEER;
-        result.direction = (options & DIRECTION_MASK) == DIRECTION_MASK ? BACKWARD : FORWARD;
+        result.side = (options & SIDE_MASK) == SIDE_MASK ? RIGHT : LEFT;
+        result.direction = (options & DIRECTION_MASK) == DIRECTION_MASK ? FORWARD : BACKWARD ;
         result.debugLevel = DEBUG_MASK & options;
         
-        //unsigned int value_as_int = value1_as_byte*16777216 + value2_as_byte*65536 + value3_as_byte*256 + value4_as_byte;
-        //int value_as_int = (*((int*) (buffer + 1)));
-        result.value = (*((int*) (buffer + 1))) / 100.f;
+        //unsigned int valueSteer_as_int = valueSteer1_as_byte*16777216 + valueSteer2_as_byte*65536 + valueSteer3_as_byte*256 + valueSteer4_as_byte;
+        //int valueSteer_as_int = (*((int*) (buffer + 1)));
+        result.valueSteer = (*((int*) (++buffer)));
+        result.valueSpeed = (*((int*) (++buffer)));
              
         return result;
     }
 };
 
 struct CarState {
-    Servo drive;
-    Servo steer;
+    Servo RIGHT;
+    Servo LEFT;
+    Servo MOVEMENT;
+
+    float RIGHT_update;
+    float LEFT_update;
+    float MOVEMENT_update;
     
-    float drive_update;
-    float steer_update;
+    bool MOVEMENT_changed;
+    bool RIGHT_changed;
+    bool LEFT_changed;
     
-    bool drive_changed;
-    bool steer_changed;
-    
-    CarState(PinName drive_pin, PinName steer_pin)
-        : drive(drive_pin), steer(steer_pin) {
+    CarState(PinName RIGHT_pin, PinName LEFT_pin, PinName MOVEMENT_pin)
+        : RIGHT(RIGHT_pin), LEFT(LEFT_pin), MOVEMENT(MOVEMENT_pin){
     }
 };
 
-CarState state(PA_12, PB_0);
+CarState state(PA_12, PB_0, PF_0);		// TODO WHICH PIN?
 
 int const BUFFER_SIZE = 5;
 uint8_t rx_buffer[BUFFER_SIZE];
@@ -58,34 +63,36 @@ uint8_t rx_buffer[BUFFER_SIZE];
 void serialReadCallback() {
     SerialInputProtocol input = SerialInputProtocol::read(rx_buffer);
     
-    if (input.mode == SerialInputProtocol::STEER) {
-        if (input.direction == SerialInputProtocol::FORWARD) {
-            state.steer_update = input.value;
-        } else {
-            state.steer_update = 1 - input.value;
+    if (input.side == SerialInputProtocol::LEFT) {
+        if (input.direction == SerialInputProtocol::FORWARD) {		// Forward Left
+            state.MOVEMENT_update = calculateSpeed(input.valueSpeed, "FORWARD");
+            state.LEFT_update = calculateSteer(input.valueSteer, "LEFT");
+        } else {													// Backwards Left
+        	state.MOVEMENT_update = calculateSpeed(input.valueSpeed, "BACKWARDS");				
+            state.LEFT_update = calculateSteer(input.valueSteer, "LEFT");				
         }
-        state.steer_changed = true;
-    } else if (input.mode == SerialInputProtocol::DRIVE) {
-        pi.putc('a');
-        pi.putc('\n');
-        if (input.direction == SerialInputProtocol::FORWARD) {
-            state.drive_update = input.value;
-        } else {
-            state.drive_update = 1 - input.value;
+    } else if (input.side == SerialInputProtocol::RIGHT) {
+        if (input.direction == SerialInputProtocol::FORWARD) {		// Forward Right
+            state.MOVEMENT_update = calculateSpeed(input.valueSpeed, "FORWARD");
+            state.RIGHT_update = calculateSteer(input.valueSteer, "RIGHT");
+        } else {													// Backwards Right
+            state.MOVEMENT_update = calculateSpeed(input.valueSpeed, "BACKWARDS");
+            state.RIGHT_update = calculateSteer(input.valueSteer, "RIGHT");	
         }
-        state.drive_changed = true;
     }
+    MOVEMENT_changed = true;
+    state.RIGHT_changed = true;
 }
 
-int rx_fuck_off = 0;
+int rx_buffer_size = 0;
 
 void rx_interrupt() {
     received_something = false;
     
-    rx_buffer[rx_fuck_off] = pi.getc();
+    rx_buffer[rx_buffer_size] = pi.getc();
     
-    rx_fuck_off = ++rx_fuck_off % BUFFER_SIZE;
-    if (rx_fuck_off == 0) {
+    rx_buffer_size = ++rx_buffer_size % BUFFER_SIZE;
+    if (rx_buffer_size == 0) {
         for (int i = 0; i < BUFFER_SIZE; ++i) {
             pi.putc(rx_buffer[i]);
         }
@@ -94,6 +101,33 @@ void rx_interrupt() {
         serialReadCallback();
         received_something = true;
     }
+}
+
+void calculateSteer(float valueSteer, string side){
+	if (valueSteer > 0.4){
+		valueSpeed=0.4;
+	}
+	if (side=="LEFT"){
+		return 0.4 - (valueSteer/10);
+	}else{
+		return 0.4 + (valueSteer/10);
+	}
+}
+
+void calculateSpeed(float valueSpeed, string direction){
+	if (valueSpeed == 0.0){
+			return 0;
+	}
+	
+	if (valueSpeed > 3){ 
+		valueSpeed = 3;
+	}
+	
+	if (direction=="FORWARD"){
+		return valueSpeed/10 + 0.5;
+	}else{
+		return valueSpeed/10;
+	}
 }
 
 int main() {
@@ -105,16 +139,23 @@ int main() {
     while(1) {
         //__disable_irq();
 
-        if (state.drive_changed) {
-            state.drive = state.drive_update;
-            state.drive_changed = false;
+        if (state.RIGHT_changed) {
+            state.RIGHT = state.RIGHT_update;
+            state.RIGHT_changed = false;
         }
-        if (state.steer_changed) {
-            state.steer = state.steer_update;
-            state.steer_changed = false;
+        if (state.LEFT_changed) {
+            state.LEFT = state.LEFT_update;
+            state.LEFT_changed = false;
         }
+        if (state.MOVEMENT_changed) {
+        	state.MOVEMENT = state.MOVEMENT_update;
+        	state.MOVEMENT_changed = false;
+        }
+
+
         //__enable_irq();
         
+
         wait_ms(wait_millis);
         
         if (received_something) {
