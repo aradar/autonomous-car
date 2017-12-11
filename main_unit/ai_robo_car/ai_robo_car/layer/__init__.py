1558ce5d81fdb1e20d2179638583d28c46170318
ichinii <sven.salt@yahoo.de>
2017-12-11 13:11:59 +0000

    removed mock layers, now using unit tests

--- main_unit/ai_robo_car/ai_robo_car/layer/__init__.py
+++ main_unit/ai_robo_car/ai_robo_car/layer/__init__.py
@@ -3,6 +3,3 @@ from ai_robo_car.layer.path_translator import PathTranslator
 from ai_robo_car.layer.object_recognizer import ObjectRecognizer
 from ai_robo_car.layer.path_finder import PathFinder
 from ai_robo_car.layer.relative_translator import RelativeTranslator
-
-from ai_robo_car.layer.mock_engine_communicator import MockEngineCommunicator
-from ai_robo_car.layer.mock_path_finder import MockPathFinder
