cc0301cc99355ab3126d055db12ff53539b08960
ichinii <sven.salt@yahoo.de>
2017-12-11 11:56:07 +0000

    fixed bugs with threshold and added unit test

--- main_unit/ai_robo_car/ai_robo_car/layer/path_translator.py
+++ main_unit/ai_robo_car/ai_robo_car/layer/path_translator.py
@@ -3,7 +3,6 @@ from ai_robo_car.layer.data_objects import TargetPoint
 from ai_robo_car.layer.data_objects import EngineInstruction
 import math
 
-
 class PathTranslator(AbstractLayer[TargetPoint, EngineInstruction]):
     def call_from_upper(self, message: TargetPoint) -> None:
         (x, y) = message.position
@@ -11,36 +10,42 @@ class PathTranslator(AbstractLayer[TargetPoint, EngineInstruction]):
 
         steer = 0
         speed = 0
-        threshold_angle = 30
-        if (x != 0) or (y != 0):
 
+        # 0 <= threshold <= 90
+        threshold_angle = 45
+
+        if y >= 0 and ((x != 0) or (y != 0)):
             if y == 0:
                 if x > 0:
-                    angle = threshold_angle
+                    angle = 0
                 else:
-                    angle = -threshold_angle
+                    angle = 180
             elif x != 0:
-                angle = (math.atan(y / x) * 180) / math.pi
+                angle = math.atan(y / x) / math.pi * 180
             else:
                 angle = 90
 
-            if angle < -90 or angle > 90:
+            if angle < 0:
+                angle = 180 + angle
+
+            # threshold
+            if angle < 0 or angle > 180:
                 print("INVALID ANGLE:", angle)
-                angle = 0
-            elif 0 > angle > -threshold_angle:
-                angle = -threshold_angle
-            elif 0 < angle < threshold_angle:
+                angle = 90
+            elif 0 <= angle < 90 - threshold_angle:
                 angle = threshold_angle
+            elif 90 + threshold_angle < angle <= 180:
+                angle = 90 + threshold_angle
 
-            print("using angle:", angle)
-
-            speed = math.fabs((angle + 90) / 90 - 1)
             if angle > 0:
                 steer = 90 - angle
             elif angle < 0:
                 steer = -90 - angle
+            speed = 1 - (math.fabs(steer) / threshold_angle)
+            speed = 0.5 + 0.5 * speed
 
         if self.lower is not None:
+            print("PathTranslator: call_lower -> speed: %d, steer: %d" % (speed, steer))
             self.call_lower(EngineInstruction(speed, steer))
 
     def call_from_lower(self, message: EngineInstruction) -> None:
