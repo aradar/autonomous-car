30b3fd3ff5cde30c80d485c0a0665f93cf2cf1ee
Bluemi <bruno.schilling@protonmail.ch>
2018-01-15 14:36:25 +0100

    + Emergency Break

--- car_controls/src/Main.cpp
+++ car_controls/src/Main.cpp
@@ -17,29 +17,30 @@ Main::Main()
 {}
 
 bool Main::emergencyStop(){
-	Timer sonar;			
+	Timer sonar;
 	int distance = 0;
-	
+	const int THRESHOLD = 35;
+
 	DigitalIn echo_one(PA_8);		// sensor 1
 	DigitalOut trig_one(PF_1);		// sensor 1
 	DigitalIn echo_two(PF_0);		// sensor 2
 	DigitalOut trig_two(PB_5);		// sensor 2
-		
+
 	trig_one = 1;					// send trigger signal for sensor 1
 	sonar.reset();
 	wait_us(10.0);
-    trig_one = 0;					
+    trig_one = 0;
     while (echo_one==0) {};			// wait for echo signal
     sonar.start();
-    while (echo_one==1) {};	
-    sonar.stop();					// stops time echo needed	
-    distance = (sonar.read_us())/58.0;	// distance between 
+    while (echo_one==1) {};
+    sonar.stop();					// stops time echo needed
+    distance = (sonar.read_us())/58.0;	// distance between
 	printf("First: %d cm \n\r", distance);
-	if(distance < 40)
+	if(distance < THRESHOLD)
 		return true;
-	
+
 	wait_us(10.0);
-	
+
 	trig_two = 1;					//send trigger signal for sensor 2
 	sonar.reset();
     wait_us(10.0);
@@ -50,9 +51,9 @@ bool Main::emergencyStop(){
     sonar.stop();
     distance = (sonar.read_us())/58.0;
 	printf("Second: %d cm \n\r", distance);
-	if(distance < 40)
+	if(distance < THRESHOLD)
 		return true;
-	
+
 	return false;
 }
 
@@ -62,23 +63,21 @@ void Main::run()
 	sequences::calibrate();
 
 	// test
-
 	wait(1);
-
 	/*
 	sequences::test_steer(steer);
 	sequences::test_drive(drive);
 	*/
 
 	int blink_counter = 0;
-	const int blink_period = 250;
+	const int blink_period = 7;
 
 	state.target_speed = 0.f;
 	state.steer_changed = true;
 
 	NetworkManager::init(state);
 
-    while(!emergencyStop()) {
+    while(1 /*!emergencyStop()*/) {
 		controller.update(drive);
 
 		// update current speed
@@ -107,7 +106,10 @@ void Main::run()
 			NetworkManager::send((float)state.side);
 		}
 
-		wait(0.01f);
+		wait(0.04f);
     }
-    drive = 0;
+
+	drive = 0;
+
+	LEDHandler::blink_blocking(0.1f, 10);
 }
